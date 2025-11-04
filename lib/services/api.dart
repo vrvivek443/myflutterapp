6e35d8e9bd06d6133f8d24fe8cbaa204d21d985e
vrvivek443 <vivek@sixofour.in>
2025-11-04 18:12:35 +0530

    Final

--- lib/services/api.dart
+++ lib/services/api.dart
@@ -1,9 +1,11 @@
+// api.dart
+
 import 'package:dio/dio.dart';
 import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
 class API {
 
-  // getUserDetails
+  // getUserDetails - Existing function to fetch user data from Microsoft Graph
   Future<Response> getUserDetails({required String token}) async {
     var dio = Dio();
     Map<String, String> headers = {'Authorization': 'Bearer $token'};
@@ -18,7 +20,7 @@ class API {
     }
   }
 
-  // getProfileImage
+  // getProfileImage - Existing function to fetch user profile image from Microsoft Graph
   Future<Response> getProfileImage({required String token}) async {
     var dio = Dio();
     Map<String, String> headers = {'Authorization': 'Bearer $token'};
@@ -34,4 +36,15 @@ class API {
     }
   }
 
+  // New function to fetch user data from your custom API (/user/get?userid={username})
+  Future<Response> getUserByUsername(String username) async {
+    var dio = Dio();
+    try {
+      final response = await dio.get('https://demo.gov-codex.com:8001/api/user/get?userid=$username');
+      return response;
+    } catch (e) {
+      print("Http Error : ${e.toString()}");
+      rethrow;
+    }
+  }
 }
